dccd6c8193468d74ee9f914a522165c5d9af4f07
Vinicius Lima <viniciusleite.desenvolvedor@gmail.com>
2019-09-08 15:25:58 -0300

    initial commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,52 +1,48 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update]
+ 
   def index
-  	@movies = Movie.all
+    @movies = Movie.all
   end
-
+ 
   def show
-  	id = params['id']
-  	@movie = Movie.find(id)
-  rescue ActiveRecord::RecordNotFound
-  	@movie = nil
-  	render file: "#{Rails.root}/public/404.html", status:404
   end
-
-  def new
-  end
-
-  def create
-  	@movie = Movie.new(movie_params)
-  	if @movie.save
-  		redirect_to action: :show, id: @movie.id
-  	else
-  	render :new	
-  	end
-  end
-
+ 
   def edit
-  	id = params['id']
-  	@movie = Movie.find(id)
-  rescue ActiveRecord::RecordNotFound
-  	@movie = nil
-  	render file: "#{Rails.root}/public/404.html", status: 404	
   end
-
+ 
   def update
-  	id = params['id']
-  	@movie = Movie.find(id)
-  	@movie.update(movie_params)
-  	if @movie.save
-  		redirect_to action: :show, id: @movie.id
-  	else
-  		render :edit, id:@movie.id
-  	end
-  	
-  rescue ActiveRecord::RecordNotFound
-  	render file: "#{Rails.root}/public/404.html", status: 404
+    @movie.update(movie_params)
+    if @movie.save
+      redirect_to action: :show, id: @movie.id
+    else
+      render :edit, id: @movie.id
+    end
   end
-
+ 
+  def new
+    @movie = Movie.new
+  end
+ 
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to action: :show, id: @movie.id
+    else
+      render :new
+    end
+  end
+ 
   private
+ 
   def movie_params
-  	params.require(:movie).permit(:title, :release_date, :description)	
+    params.require(:movie).permit(:title, :release_date, :description)
+  end
+ 
+  def set_movie
+    id = params['id']
+    @movie = Movie.find(id)
+  rescue ActiveRecord::RecordNotFound
+    render file: "#{Rails.root}/public/404.html", status: 404
   end
 end
